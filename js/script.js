24b171d665be144e92f9c1a8d20606d04ad9a7fb
MuellerMarcus <marcus.mm.mueller@gmail.com>
2021-11-23 20:30:00 +0000

    Dummy Json angelegt

--- js/script.js
+++ js/script.js
@@ -1,8 +1,36 @@
 // Backend
 setURL('https://gruppe-130.developerakademie.net/smallest_backend_ever');
 
+let tasks = [{
+    'id': 0,
+    'name': 'Anna',
+    'title': 'Add Task Ready machen',
+    'dueDate': '26.11.2021',
+    'category': 'project',
+    'urgency': 'high',
+    'description': 'Logik hinter Add to Task ist implementiert und wird auf diesen JSON gespeichert',
+    'status': 'none'
+}, {
+    'id': 1,
+    'name': 'Marcus',
+    'title': 'Backlog Ready machen',
+    'dueDate': '29.11.2021',
+    'category': 'project',
+    'urgency': 'high',
+    'description': 'Logik hinter backlog ist implementiert und wird aus JSON gezogen/ von Add to Task übergeben',
+    'status': 'bl'
+}, {
+    'id': 2,
+    'name': 'Daniel',
+    'title': 'Board Ready machen',
+    'dueDate': '29.11.2021',
+    'category': 'project',
+    'urgency': 'high',
+    'description': 'Logik hinter Board ist implementiert und wird aus JSON gezogen / von Backlog übergeben',
+    'status': 'b1'
+}];
 
-
+//status: bl= Backlog ; b1=board - todo ; b2=board - in Progress ; b3=board - testing; b4= board Done 
 
 async function init() {
     await downloadFromServer();
