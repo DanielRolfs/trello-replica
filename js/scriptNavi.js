4427fb456e08a0af95b43e3dac4c334803e78160
MuellerMarcus <marcus.mm.mueller@gmail.com>
2021-11-23 17:44:06 +0000

    about und privacy

--- js/scriptNavi.js
+++ js/scriptNavi.js
@@ -11,6 +11,11 @@ function showBoard() {
     document.getElementById('includeHelp').classList.add('d-none');
     document.getElementById('help').classList.remove('aktivsidebarlink');
 
+    document.getElementById('includeAbout').classList.add('d-none');
+
+    document.getElementById('includePrivacy').classList.add('d-none');
+
+
 }
 
 function showBacklog() {
@@ -55,5 +60,45 @@ function showHelp() {
 
     document.getElementById('includeHelp').classList.remove('d-none');
     document.getElementById('help').classList.add('aktivsidebarlink');
+
+}
+
+function showAbout() {
+    document.getElementById('includeBoard').classList.add('d-none');
+    document.getElementById('board').classList.remove('aktivsidebarlink');
+
+    document.getElementById('includeBacklog').classList.add('d-none');
+    document.getElementById('backlog').classList.remove('aktivsidebarlink');
+
+    document.getElementById('includeAddtask').classList.add('d-none');
+    document.getElementById('addtask').classList.remove('aktivsidebarlink');
+
+    document.getElementById('includeHelp').classList.add('d-none');
+    document.getElementById('help').classList.remove('aktivsidebarlink');
+
+    document.getElementById('includeAbout').classList.remove('d-none');
+
+    document.getElementById('includePrivacy').classList.add('d-none');
+
+
+}
+
+function showPrivacy() {
+    document.getElementById('includeBoard').classList.add('d-none');
+    document.getElementById('board').classList.remove('aktivsidebarlink');
+
+    document.getElementById('includeBacklog').classList.add('d-none');
+    document.getElementById('backlog').classList.remove('aktivsidebarlink');
+
+    document.getElementById('includeAddtask').classList.add('d-none');
+    document.getElementById('addtask').classList.remove('aktivsidebarlink');
+
+    document.getElementById('includeHelp').classList.add('d-none');
+    document.getElementById('help').classList.remove('aktivsidebarlink');
+
+    document.getElementById('includeAbout').classList.add('d-none');
+
+    document.getElementById('includePrivacy').classList.remove('d-none');
+
 
 }
