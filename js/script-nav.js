c952f074c86fb5fd251d71443f826108b90b1da8
announa <moupou@posteo.org>
2021-11-29 14:19:27 -0500

    script-nav.js corrected

--- js/script-nav.js
+++ js/script-nav.js
@@ -1,82 +1,25 @@
 function showBoard() {
     document.getElementById('include-board').classList.remove('d-none');
     document.getElementById('board').classList.add('sidebarlink--active');
-<<<<<<< HEAD
-    document.getElementById('include-backlog').classList.add('d-none');
-    document.getElementById('backlog').classList.remove('sidebarlink--active');
-    document.getElementById('include-addtask').classList.add('d-none');
-    document.getElementById('addtask').classList.remove('sidebarlink--active');
-    document.getElementById('include-help').classList.add('d-none');
-    document.getElementById('help').classList.remove('sidebarlink--active');
-    document.getElementById('include-about').classList.add('d-none');
-    document.getElementById('include-privacy').classList.add('d-none');
-    document.getElementById('include-adduser').classList.add('d-none');
-
-=======
->>>>>>> c9b2bddef0044fb680d500ea2fb26ccfa6fcc959
 }
 
 function showBacklog() {
    document.getElementById('include-backlog').classList.remove('d-none');
     document.getElementById('backlog').classList.add('sidebarlink--active');
-<<<<<<< HEAD
-
-    document.getElementById('include-addtask').classList.add('d-none');
-    document.getElementById('addtask').classList.remove('sidebarlink--active');
-
-    document.getElementById('include-help').classList.add('d-none');
-    document.getElementById('help').classList.remove('sidebarlink--active');
-
-    document.getElementById('include-about').classList.add('d-none');
-
-    document.getElementById('include-privacy').classList.add('d-none');
-    document.getElementById('include-adduser').classList.add('d-none');
-
-=======
->>>>>>> c9b2bddef0044fb680d500ea2fb26ccfa6fcc959
 }
 
 function showAddTask() {
    document.getElementById('include-addtask').classList.remove('d-none');
     document.getElementById('addtask').classList.add('sidebarlink--active');
-<<<<<<< HEAD
-
-    document.getElementById('include-help').classList.add('d-none');
-    document.getElementById('help').classList.remove('sidebarlink--active');
-
-    document.getElementById('include-about').classList.add('d-none');
-
-    document.getElementById('include-privacy').classList.add('d-none');
-    document.getElementById('include-adduser').classList.add('d-none');
-
-=======
->>>>>>> c9b2bddef0044fb680d500ea2fb26ccfa6fcc959
 }
 
 function showHelp() {
     document.getElementById('include-help').classList.remove('d-none');
     document.getElementById('help').classList.add('sidebarlink--active');
-<<<<<<< HEAD
-
-    document.getElementById('include-about').classList.add('d-none');
-
-    document.getElementById('include-privacy').classList.add('d-none');
-    document.getElementById('include-adduser').classList.add('d-none');
-
-=======
->>>>>>> c9b2bddef0044fb680d500ea2fb26ccfa6fcc959
 }
 
 function showAbout() {
     document.getElementById('include-about').classList.remove('d-none');
-<<<<<<< HEAD
-
-    document.getElementById('include-privacy').classList.add('d-none');
-    document.getElementById('include-adduser').classList.add('d-none');
-
-
-=======
->>>>>>> c9b2bddef0044fb680d500ea2fb26ccfa6fcc959
 }
 
 function showPrivacy() {
@@ -93,35 +36,5 @@ function hideboard(){
     document.getElementById('include-help').classList.add('d-none');
     document.getElementById('help').classList.remove('sidebarlink--active');
     document.getElementById('include-about').classList.add('d-none');
-<<<<<<< HEAD
-
-    document.getElementById('include-privacy').classList.remove('d-none');
-    document.getElementById('include-adduser').classList.add('d-none');
-
-
-}
-
-function showadduser() {
-    document.getElementById('include-board').classList.add('d-none');
-    document.getElementById('board').classList.remove('sidebarlink--active');
-
-    document.getElementById('include-backlog').classList.add('d-none');
-    document.getElementById('backlog').classList.remove('sidebarlink--active');
-
-    document.getElementById('include-addtask').classList.add('d-none');
-    document.getElementById('addtask').classList.remove('sidebarlink--active');
-
-    document.getElementById('include-help').classList.add('d-none');
-    document.getElementById('help').classList.remove('sidebarlink--active');
-
-    document.getElementById('include-about').classList.add('d-none');
-
     document.getElementById('include-privacy').classList.add('d-none');
-
-    document.getElementById('include-adduser').classList.remove('d-none');
-
-
-=======
-    document.getElementById('include-privacy').classList.add('d-none');
->>>>>>> c9b2bddef0044fb680d500ea2fb26ccfa6fcc959
 }
