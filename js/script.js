ffb46665a0b39e8a060eca30ad74c0d462a5e763
MuellerMarcus <marcus.mm.mueller@gmail.com>
2021-12-01 02:05:44 +0000

    resp navi im backlog

--- js/script.js
+++ js/script.js
@@ -36,13 +36,27 @@ async function loadTasks() {
     }
 }
 
+
+//sidebar
 function showActiveLink() {
-  let links = Array.from(document.links);
-  let activeLink = links.find((link) => document.URL.includes(link.href));
-  removeLinkMarker(links);
-  activeLink.parentElement.classList.add('sidebarlink--active');
+    let links = Array.from(document.links);
+    let activeLink = links.find((link) => document.URL.includes(link.href));
+    removeLinkMarker(links);
+    activeLink.parentElement.classList.add('sidebarlink--active');
 }
 
 function removeLinkMarker(links) {
-  links.forEach((link) => link.parentElement.classList.remove('sidebarlink--active'));
+    links.forEach((link) => link.parentElement.classList.remove('sidebarlink--active'));
+}
+
+function dropout() {
+    document.getElementById('sidebar').style.left = '0px';
+    document.getElementById('navi-menu').classList.add('d-none');
+    document.getElementById('navi-close').classList.remove('d-none');
+}
+
+function dropin() {
+    document.getElementById('sidebar').style.left = '-130px';
+    document.getElementById('navi-menu').classList.remove('d-none');
+    document.getElementById('navi-close').classList.add('d-none');
 }
