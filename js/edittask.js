43820ff1039d39df527a46599bbbddf159e62b79
announa <moupou@posteo.org>
2021-11-30 22:06:59 -0500

    continued edit task

--- js/edittask.js
+++ js/edittask.js
@@ -1,7 +1,6 @@
 function editTask(taskId) {
   let task = getTask(taskId);
   openTaskEditor();
-  /* changeSubmitEvent(task) */
   loadTaskToForm(task);
 }
 
@@ -13,27 +12,13 @@ function openTaskEditor() {
   document.querySelector('.edit-task-modal').classList.remove('d-none');
 }
 
-/* function changeSubmitEvent(task){
-  let form = document.getElementById('add-task__form')
-  form.removeAttribute('onsubmit');
-  /* form.removeEventListener('submit', ()=> {addTask(); return false})
-  form.addEventListener('submit', () => {saveChanges(task); return false})
-
-  document.getElementById('save-task-btn').innerHTML = 'Save Changes';
-} */
-
 function loadTaskToForm(task) {
- let title = document.getElementById('edit-task__title');
- let category = document.getElementById('edit-task__category');
- let description = document.getElementById('edit-task__description');
- let dueDate = document.getElementById('edit-task__date');
- let urgency = document.getElementById('edit-task__urgency');
 
- title.value = task.title;
- category.value = task.category;
- description.value = task.description;
- dueDate.value = task.dueDate;
- urgency.value = task.urgency;
+ getFormField('title').value = task.title;
+ getFormField('category').value = task.category;
+ getFormField('description').value = task.description;
+ getFormField('date').value = task.dueDate;
+ getFormField('urgency').value = task.urgency;
 
  getResponsibles(task);
 }
@@ -46,18 +31,22 @@ function getResponsibles(task){
 }
 
 function saveChanges(task){
-  let title = document.getElementById('edit-task__title');
-  let category = document.getElementById('edit-task__category');
-  let description = document.getElementById('edit-task__description');
-  let dueDate = document.getElementById('edit-task__date');
-  let urgency = document.getElementById('edit-task__urgency');
 
-  task.title = title.value;
-  task.category = category.value;
-  task.description = description.value;
-  task.dueDate = dueDate.value;
-  task.urgency = urgency.value;
+  task.title = getFormField('title').value;
+  task.category = getFormField('category').value;
+  task.description = getFormField('description').value;
+  task.dueDate = getFormField('date').value;
+  task.urgency = getFormField('urgency').value;
   task.responsible = getResponsibleId();
 
   saveTask();
+}
+
+function cancelEditTask(){
+  document.querySelector('.edit-task-modal').classList.add('d-none');
+  resetVariables();
+}
+
+function getFormField(field){
+  return document.getElementById('edit-task__'+field);
 }
