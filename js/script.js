fec333968ca92d982a0de320004d3f62ea0e22a3
announa <moupou@posteo.org>
2021-12-03 16:18:07 -0500

    navbar responsiv finished

--- js/script.js
+++ js/script.js
@@ -3,108 +3,94 @@ setURL('http://gruppe-130.developerakademie.net/smallest_backend_ever');
 
 let tasks = [];
 
-let users = [{
-        username: 'Anna',
-        password: 'anna12345',
-        image: './img/profil1.png',
-        mail: 'anna@da.de',
-        tel: '001200321321',
-        id: 1,
-    },
-    {
-        username: 'Marcus',
-        password: 'marcus12345',
-        image: './img/marcus.jpg',
-        mail: 'marcus@da.de',
-        tel: '001200321321',
-        id: 2,
-    },
-    {
-        username: 'Daniel',
-        password: 'daniel12345',
-        image: './img/profil2.png',
-        mail: 'daniel@da.de',
-        tel: '001200321321',
-        id: 3,
-    },
-    {
-        username: 'This is a long user name',
-        password: 'daniel12345',
-        image: './img/profil2.png',
-        mail: 'thisisalongusermailaddress@da.de',
-        tel: '001200321321',
-        id: 4,
-    },
+let users = [
+  {
+    username: 'Anna',
+    password: 'anna12345',
+    image: './img/profil1.png',
+    mail: 'anna@da.de',
+    tel: '001200321321',
+    id: 1,
+  },
+  {
+    username: 'Marcus',
+    password: 'marcus12345',
+    image: './img/marcus.jpg',
+    mail: 'marcus@da.de',
+    tel: '001200321321',
+    id: 2,
+  },
+  {
+    username: 'Daniel',
+    password: 'daniel12345',
+    image: './img/profil2.png',
+    mail: 'daniel@da.de',
+    tel: '001200321321',
+    id: 3,
+  },
+  {
+    username: 'This is a long user name',
+    password: 'daniel12345',
+    image: './img/profil2.png',
+    mail: 'thisisalongusermailaddress@da.de',
+    tel: '001200321321',
+    id: 4,
+  },
 ];
 
 //status: bl= Backlog ; b1=board - todo ; b2=board - in Progress ; b3=board - testing; b4= board Done
 
-
 async function loadTasks() {
-    await downloadFromServer();
-    if (backend.getItem('tasks')) {
-        tasks = JSON.parse(backend.getItem('tasks'));
-    }
+  await downloadFromServer();
+  if (backend.getItem('tasks')) {
+    tasks = JSON.parse(backend.getItem('tasks'));
+  }
 }
-
 
 //sidebar
 function showActiveLink() {
-    let links = Array.from(document.links);
-    let activeLink = links.find((link) => document.URL.includes(link.href));
-    removeLinkMarker(links);
-    activeLink.parentElement.classList.add('sidebarlink--active');
+  let links = Array.from(document.links);
+  let activeLink = links.find((link) => document.URL.includes(link.href));
+  removeLinkMarker(links);
+  activeLink.parentElement.classList.add('sidebarlink--active');
 }
 
 function removeLinkMarker(links) {
-    links.forEach((link) => link.parentElement.classList.remove('sidebarlink--active'));
+  links.forEach((link) => link.parentElement.classList.remove('sidebarlink--active'));
 }
 
-
+// responsive nav-bar
 
 function dropout() {
-    document.getElementById('sidebar__links').style.visibility = 'visible';
-    document.getElementById('sidebar__bottom').style.visibility = 'visible';
-    document.getElementById('sidebar').classList.add('sidebar-open');
-    document.getElementById('navi-menu').style.visibility = 'hidden';
-    document.getElementById('navi-close').style.visibility = 'visible';
-    document.getElementById('x700').style.visibility = 'hidden';
-    document.getElementById('menu700').style.visibility = 'hidden';
-
+  document.getElementById('sidebar').classList.add('sidebar-open');
+  toggleBtnVisibility();
+  toggleNavContentVisibility();
 }
 
 function dropin() {
-    document.getElementById('sidebar__links').style.visibility = 'hidden';
-    document.getElementById('sidebar__bottom').style.visibility = 'hidden';
-    document.getElementById('sidebar').classList.remove('sidebar-open');
-
-    // document.getElementById('sidebar__links').style.visibility = 'hidden';
-    // document.getElementById('sidebar__bottom').style.visibility = 'hidden';
-    // document.getElementById('sidebar').style.left = '-100px';
-    document.getElementById('navi-menu').style.visibility = 'visible';
-    document.getElementById('navi-close').style.visibility = 'hidden';
-    // document.getElementById('x700').style.visibility = 'hidden';
-    // document.getElementById('menu700').style.visibility = 'hidden';
+  document.getElementById('sidebar').classList.remove('sidebar-open');
+  toggleBtnVisibility();
+  setTimeout(() => {
+    toggleNavContentVisibility();
+  }, 220);
 }
 
-// function dropout700() {
-//     document.getElementById('sidebar__links').style.visibility = 'visible';
-//     document.getElementById('sidebar__bottom').style.visibility = 'visible';
-//     document.getElementById('sidebar').style.left = '0px';
-//     document.getElementById('navi-menu').classList.add('d-none');
-//     document.getElementById('navi-close').classList.add('d-none');
-//     document.getElementById('x700').style.visibility = 'visible';
-//     document.getElementById('menu700').style.visibility = 'hidden';
+function toggleBtnVisibility() {
+  let navBtns = Array.from(document.getElementsByClassName('nav-btn'));
+  navBtns.forEach((btn) => btn.classList.toggle('v-hidden'));
+}
 
-// }
+function toggleNavContentVisibility() {
+  let navContent = Array.from(document.getElementsByClassName('sidebar__content'));
+  navContent.forEach(e => e.classList.toggle('v-visible'));
+}
 
-// function dropin700() {
-//     document.getElementById('sidebar__links').style.visibility = 'hidden';
-//     document.getElementById('sidebar__bottom').style.visibility = 'hidden';
-//     document.getElementById('sidebar').style.left = '-150px';
-//     document.getElementById('navi-menu').classList.add('d-none');
-//     document.getElementById('navi-close').classList.add('d-none');
-//     document.getElementById('x700').style.visibility = 'hidden';
-//     document.getElementById('menu700').style.visibility = 'visible';
+window.onresize = function() {
+  if (window.innerWidth > 1000 && navBarIsOpen()) {
+      dropin()
+  }
+}
 
-// }
+function navBarIsOpen() {
+  return document.getElementById('sidebar').classList.contains('sidebar-open');
+}
