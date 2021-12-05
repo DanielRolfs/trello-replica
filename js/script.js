210848e54a6cc7e65976bff97db7912cd75db8f0
Marcus Müller <93290039+MuellerMarcus@users.noreply.github.com>
2021-12-05 21:33:53 +0000

    Legend in Extrabar (over all) + Search Funktion

--- js/script.js
+++ js/script.js
@@ -3,94 +3,118 @@ setURL('http://gruppe-130.developerakademie.net/smallest_backend_ever');
 
 let tasks = [];
 
-let users = [
-  {
-    username: 'Anna',
-    password: 'anna12345',
-    image: './img/profil1.png',
-    mail: 'anna@da.de',
-    tel: '001200321321',
-    id: 1,
-  },
-  {
-    username: 'Marcus',
-    password: 'marcus12345',
-    image: './img/marcus.jpg',
-    mail: 'marcus@da.de',
-    tel: '001200321321',
-    id: 2,
-  },
-  {
-    username: 'Daniel',
-    password: 'daniel12345',
-    image: './img/profil2.png',
-    mail: 'daniel@da.de',
-    tel: '001200321321',
-    id: 3,
-  },
-  {
-    username: 'This is a long user name',
-    password: 'daniel12345',
-    image: './img/profil2.png',
-    mail: 'thisisalongusermailaddress@da.de',
-    tel: '001200321321',
-    id: 4,
-  },
+let users = [{
+        username: 'Anna',
+        password: 'anna12345',
+        image: './img/profil1.png',
+        mail: 'anna@da.de',
+        tel: '001200321321',
+        id: 1,
+    },
+    {
+        username: 'Marcus',
+        password: 'marcus12345',
+        image: './img/marcus.jpg',
+        mail: 'marcus@da.de',
+        tel: '001200321321',
+        id: 2,
+    },
+    {
+        username: 'Daniel',
+        password: 'daniel12345',
+        image: './img/profil2.png',
+        mail: 'daniel@da.de',
+        tel: '001200321321',
+        id: 3,
+    },
+    {
+        username: 'This is a long user name',
+        password: 'daniel12345',
+        image: './img/profil2.png',
+        mail: 'thisisalongusermailaddress@da.de',
+        tel: '001200321321',
+        id: 4,
+    },
 ];
 
 //status: bl= Backlog ; b1=board - todo ; b2=board - in Progress ; b3=board - testing; b4= board Done
 
 async function loadTasks() {
-  await downloadFromServer();
-  if (backend.getItem('tasks')) {
-    tasks = JSON.parse(backend.getItem('tasks'));
-  }
+    await downloadFromServer();
+    if (backend.getItem('tasks')) {
+        tasks = JSON.parse(backend.getItem('tasks'));
+    }
 }
 
 //sidebar
 function showActiveLink() {
-  let links = Array.from(document.links);
-  let activeLink = links.find((link) => document.URL.includes(link.href));
-  removeLinkMarker(links);
-  activeLink.parentElement.classList.add('sidebarlink--active');
+    let links = Array.from(document.links);
+    let activeLink = links.find((link) => document.URL.includes(link.href));
+    removeLinkMarker(links);
+    activeLink.parentElement.classList.add('sidebarlink--active');
 }
 
 function removeLinkMarker(links) {
-  links.forEach((link) => link.parentElement.classList.remove('sidebarlink--active'));
+    links.forEach((link) => link.parentElement.classList.remove('sidebarlink--active'));
 }
+//sidebarend
 
 // responsive nav-bar
 
 function dropout() {
-  document.getElementById('sidebar').classList.add('sidebar-open');
-  toggleBtnVisibility();
-  toggleNavContentVisibility();
+    document.getElementById('sidebar').classList.add('sidebar-open');
+    toggleBtnVisibility();
+    toggleNavContentVisibility();
 }
 
 function dropin() {
-  document.getElementById('sidebar').classList.remove('sidebar-open');
-  toggleBtnVisibility();
-  setTimeout(() => {
-    toggleNavContentVisibility();
-  }, 150);
+    document.getElementById('sidebar').classList.remove('sidebar-open');
+    toggleBtnVisibility();
+    setTimeout(() => {
+        toggleNavContentVisibility();
+    }, 150);
 }
 
 function toggleBtnVisibility() {
-  let navBtns = Array.from(document.getElementsByClassName('nav-btn'));
-  navBtns.forEach((btn) => btn.classList.toggle('v-hidden'));
+    let navBtns = Array.from(document.getElementsByClassName('nav-btn'));
+    navBtns.forEach((btn) => btn.classList.toggle('v-hidden'));
 }
 
 function toggleNavContentVisibility() {
-  let navContent = Array.from(document.getElementsByClassName('sidebar__content'));
-  navContent.forEach(e => e.classList.toggle('v-visible'));
+    let navContent = Array.from(document.getElementsByClassName('sidebar__content'));
+    navContent.forEach(e => e.classList.toggle('v-visible'));
 }
 
 window.onresize = function() {
-  if (window.innerWidth > 1000 && navBarIsOpen()) {
-      dropin()
-  }
+    if (window.innerWidth > 1000 && navBarIsOpen()) {
+        dropin()
+    }
 }
 
 function navBarIsOpen() {
-  return document.getElementById('sidebar').classList.contains('sidebar-open');
+    return document.getElementById('sidebar').classList.contains('sidebar-open');
 }
+// responsive nav-bar end
+
+
+// Searchbar
+
+function showSearch() {
+    document.getElementById('search-input').style.visibility = 'visible';
+    document.getElementById('search-input').style.width = '200px';
+    document.getElementById('search-btn').setAttribute("onClick", "hideSearch();");
+}
+
+function hideSearch() {
+    document.getElementById('search-input').style.visibility = 'hidden';
+    document.getElementById('search-input').style.width = '0px';
+    document.getElementById('search-btn').setAttribute("onClick", "showSearch();");
+}
+
+function filterTasks() {
+    let search = document.getElementById('search-input').value;
+    search = search.toLowerCase();
+
+}
+
+// Searchbar END
