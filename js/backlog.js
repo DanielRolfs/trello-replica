8209ffb2d2b9ea4338de0cd6c7f0b03d72f022d5
Marcus Müller <93290039+MuellerMarcus@users.noreply.github.com>
2021-12-09 18:43:26 +0000

    Design Backlog

--- js/backlog.js
+++ js/backlog.js
@@ -17,13 +17,38 @@ async function loadBacklogs() {
 
 function generateLogs(log) {
     return `
-    <tr  class="log rendered-task ${log.category}" id="log" >
-        <td id="first-field"  id="cat-date-urg" class="${log.category}"><div class=" bl-board bounce next" ><img src="img/next.png" alt="" onclick="pushTaskToBoard(${log.id}) " </div></td>
-        <td id="bl-users${log.id}" class="users td-width" onclick="editTask(${log.id})"></td>
-        <td id="third-field" onclick="editTask(${log.id})"><div class="mrl-15 point center row700" >${log.title}</div></td>
-        <td id="fourth-field" class="td-width row700" onclick="editTask(${log.id})"><div class="mrl-15 fw-italic point gap row700" ><div class="center" ><div>${log.category}</div><div class="fw-normal bl-date pd700">${log.dueDate}</div><div class="prio ${log.urgency}">${log.urgency}</div></div></td>
-        <td id ="fifth-field" onclick="editTask(${log.id})"><div class="mrl-15 fw-normal point center" >${log.description}</div></td>
-        <td id="last-field"><div class="del"><img src="./img/delete1.png" alt="delete assginment" class="delete-assignment-btn__icon" onclick="deleteTask(${log.id})"> </div></td>
+    <tr  class="log rendered-task ${log.category}  filter-prio--${log.urgency}" id="log" >
+            <td id="bl-users${log.id}" class="users td-width ${log.category}" onclick="editTask(${log.id})"></td>
+
+            <td id="third-field" onclick="editTask(${log.id})">
+                <div class="ml-20 point center row700" >${log.title}</div>
+            </td>
+            
+            <td id="fourth-field" class="td-width row700" onclick="editTask(${log.id})">
+                <div class="mrl-15 fw-italic point gap row700 center fw-normal resp-cat" >
+                  <div>${log.category}</div>
+            </td>
+
+            <td id="first-field" >
+            <div class="urg-date">
+                <div class="fw-normal bl-date pd700">${log.dueDate}</div>
+                <div class="prio ${log.urgency}">${log.urgency}</div>
+            </div>
+                </td>
+
+            <td id ="fifth-field" onclick="editTask(${log.id})">
+                <div class="mrl-15 fw-normal point center resp-desc" >${log.description}</div>
+            </td>
+
+            <td id="last-field" >
+                <div class="del">
+                <span id="sign-to-board">
+                <div class=" bl-board bounce next" > <img src="img/next.png" alt="" onclick="pushTaskToBoard(${log.id})  id="to-board"></div>
+                    <p>to <br> Board</p>
+                    </span>
+                    <img src="./img/delete1.png" alt="delete assginment" class="delete-assignment-btn__icon" onclick="deleteTask(${log.id})" id="del-btn-bl">
+                </div>
+            </td>
     </tr>
   `
 }
