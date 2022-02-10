ed8fc98e9d7d05c0412fbfb208491f9e881cf52b
Daniel Rolfs <danielrolfs@icloud.com>
2022-02-10 09:43:50 +0100

    refactor: ...

--- js/backlog.js
+++ js/backlog.js
@@ -24,23 +24,23 @@ async function renderTasksInBacklog() {
 function generateLogs(log) {
     return `
     <tr  class="log rendered-task ${log.category}  filter-prio--${log.urgency}" id="task-${log.id}" >
-    <td id="bl-users${log.id}" class="users td-width ${log.category}" onclick="editTask(${log.id})"></td>
+    <td id="bl-users${log.id}" class="users td-width" onclick="editTask(${log.id})"></td>
 
     <td id="third-field" onclick="editTask(${log.id})">
         <div class="point row700 short-title" >${log.title}</div>
     </td>
     
     <td id="fourth-field" class="td-width row700" onclick="editTask(${log.id})">
-        <div class="mrl-15 fw-italic point gap row700 center fw-normal resp-cat" >
-          <div>${log.category}</div>
+        <div class="mrl-15 fw-italic point gap row700 center fw-normal resp-cat " >
+          <div class="${log.category}">${log.category}</div>
     </td>
 
     <td id="first-field" >
-    <div class="urg-date">
-        <div class="fw-normal bl-date pd700">${log.dueDate}</div>
-        <div class="prio ${log.urgency}">${log.urgency}</div>
-    </div>
-        </td>
+        <div class="urg-date">
+            <div class="fw-normal bl-date pd700">${log.dueDate}</div>
+            <div class="prio ${log.urgency}">${log.urgency}</div>
+        </div>
+    </td>
 
     <td id ="fifth-field" onclick="editTask(${log.id})">
         <div class="mrl-15 fw-normal point  resp-desc short-desc" >${log.description}</div>
